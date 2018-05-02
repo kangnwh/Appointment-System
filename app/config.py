2a9b289f7a9a4004b0b6c16a02fc7ff9bee2dbfc
kangnwh <hnzz.kn@gmail.com>
2018-05-02 12:02:11 +1000

    using base dir

--- app/config.py
+++ app/config.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
+import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DEBUG = True
 SQLALCHEMY_ECHO = False
-SQLALCHEMY_DATABASE_URI = 'sqlite://///Users/eric/Documents/projects/MyDWTool/app/app.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:////%s/app/app.db' % BASE_DIR
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = '\xff\x93\xc8w\x13\x0e3\xd6\x82\x0f\x84\x18\xe7\xd9\\|\x04e\xb9(\xfd\xc3'
 ADMIN_USER = 'admin'
